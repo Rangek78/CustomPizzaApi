9686f2268f30c838f8a151a7d65234efce03d74d
Lucas Rangel <l.rangel78@hotmail.com>
2026-05-26 21:36:23 -0300

    update database

---
+++ Migrations/20260527003545_ingredient in pizza.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomPizzaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ingredientinpizza : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientInPizza_Ingredient_IngredientId",
+                table: "IngredientInPizza");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ingredient",
+                table: "Ingredient");
+
+            migrationBuilder.RenameTable(
+                name: "Ingredient",
+                newName: "Ingredients");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ingredients",
+                table: "Ingredients",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientInPizza_Ingredients_IngredientId",
+                table: "IngredientInPizza",
+                column: "IngredientId",
+                principalTable: "Ingredients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_IngredientInPizza_Ingredients_IngredientId",
+                table: "IngredientInPizza");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Ingredients",
+                table: "Ingredients");
+
+            migrationBuilder.RenameTable(
+                name: "Ingredients",
+                newName: "Ingredient");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Ingredient",
+                table: "Ingredient",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_IngredientInPizza_Ingredient_IngredientId",
+                table: "IngredientInPizza",
+                column: "IngredientId",
+                principalTable: "Ingredient",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
