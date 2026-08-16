da08ac386a29bce852675fb9bb6a3219044fe20e
Lucas Rangel <l.rangel78@hotmail.com>
2026-08-16 19:54:44 -0300

    Simplify project structure; Create first integration tests for the API

---
+++ CustomPizzaApi.Tests/PizzaControllerIntegrationTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Net.Http.Json;
+using CustomPizzaApi.Data;
+using CustomPizzaApi.Data.Dtos.Pizza;
+using Microsoft.Extensions.DependencyInjection;
+using CustomPizzaApi.Models;
+using System.Net;
+
+namespace CustomPizzaApi.Tests;
+
+public class PizzaControllerIntegrationTests : IClassFixture<PizzaWebApplicationFactory>
+{
+    private readonly PizzaWebApplicationFactory app;
+
+    public PizzaControllerIntegrationTests(PizzaWebApplicationFactory app) {
+        this.app = app;
+    }
+
+    [Fact]
+    public async Task GetAllPizzas_Returns200()
+    {
+        // Arrange
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<PizzaContext>();
+        var pizza = new Pizza
+        {
+            Name = "Margherita",
+            Size = PizzaSize.Medium,
+            Ingredients = []
+        };
+        context.Pizzas.Add(pizza);
+        await context.SaveChangesAsync();
+
+        var client = app.CreateClient();
+
+        // Act
+        var result = await client.GetAsync("api/pizzas/");
+        var pizzaList = await result.Content.ReadFromJsonAsync<IEnumerable<ReadPizzaDto>>();
+
+        // Assert
+        Assert.True(result.IsSuccessStatusCode);
+        Assert.NotNull(pizzaList);
+    }
+
+    [Fact]
+    public async Task GetById_Returns200_WhenPizzaExists()
+    {
+        // Arrange
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<PizzaContext>();
+        var pizza = new Pizza
+        {
+            Name = "Margherita",
+            Size = PizzaSize.Small,
+            Ingredients = []
+        };
+        context.Pizzas.Add(pizza);
+        await context.SaveChangesAsync();
+
+        var client = app.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"api/pizzas/{pizza.Id}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var result = await response.Content.ReadFromJsonAsync<ReadPizzaDto>();
+
+        Assert.NotNull(result);
+        Assert.Equal(pizza.Id, result.Id);
+        Assert.Equal(pizza.Name, result.Name);
+        Assert.Equal(pizza.Size, result.Size);
+        Assert.Equivalent(pizza.Ingredients, result.Ingredients);
+    }
+
+    [Fact]
+    public async Task GetById_Returns400_WhenPizzaDoesNotExist()
+    {
+        // Arrange
+        var client = app.CreateClient();
+
+        // Act
+        var response = await client.GetAsync($"api/pizzas/0");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
---
+++ CustomPizzaApi.Tests/PizzaWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using CustomPizzaApi.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomPizzaApi.Tests;
+
+public class PizzaWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices((context, services) =>
+        {
+            string? connectionString = context.Configuration["ConnectionStrings:PizzaTestsConnection"];
+            if (string.IsNullOrEmpty(connectionString)) throw new InvalidOperationException("Test database connection string not found in configuration.");
+
+            services.RemoveAll<DbContextOptions<PizzaContext>>();
+
+            services.AddDbContext<PizzaContext>(options =>
+            {
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+        });
+    }
+
+    async Task IAsyncLifetime.InitializeAsync()
+    {
+        using var scope = Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<PizzaContext>();
+        Console.WriteLine(context.Database.ProviderName);
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.MigrateAsync();
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        Console.WriteLine("Disposed");
+    }
+}
